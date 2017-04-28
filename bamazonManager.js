a8ed55830399529de695931ff4efe147be3785db
Alek Shnayder <alek.shnayder@gmail.com>
2017-04-27 19:53:14 -0500

    Initial interview and switch

--- bamazonManager.js
+++ bamazonManager.js
@@ -14,3 +14,66 @@ connection.connect( (err) => {
 	if (err) throw err;
 	console.log(chalk.grey('connected to database as ID: ' + connection.threadId));
 });
+
+
+
+
+//interview - user to select action
+inquirer.prompt([
+	{
+		type: 'list',
+		message: 'Select Action:',
+		choices: [
+			'View Products for Sale', 
+			'View Low Inventory',
+			'Add to Inventory',
+			'Add to Product',
+		],
+		name: 'selected'
+	}
+]).then( (choice)=>{
+	console.log(choice.selected);
+	switch(choice.selected){
+		case 'View Products for Sale':
+			viewProducts();
+			break;
+
+		case 'View Low Inventory':
+			viewLowInventory()
+			break;
+
+		case 'Add to Inventory':
+			addToInventory();
+			break;
+
+		case 'Add to Product':
+			addToProduct();
+			break;
+	}
+});
+
+
+
+//===FUNCTIONS==
+function viewProducts(){
+	console.log('viewProducts');
+	connection.end();
+}
+
+
+function viewLowInventory(){
+
+}
+
+
+function addToInventory(){
+
+}
+
+
+function addToProduct(){
+
+}
+
+
+
